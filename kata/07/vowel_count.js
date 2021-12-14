1b720119fca31c922a1cbe9f28d8cade2d6b097c
Ju <j.semeniuk.v@gmail.com>
2021-12-14 10:33:51 +0200

    kata 07 vowel count

---
+++ kata/07/vowel_count.js
@@ -0,0 +1,24 @@
+//https://www.codewars.com/kata/54ff3102c1bad923760001f3
+
+function countVowels(string) {
+  let array = string.toLowerCase().split("");
+  let i = 0;
+  let arrayVowel = [];
+
+  while (i < array.length) {
+    if (
+      array[i] === "a" ||
+      array[i] === "e" ||
+      array[i] === "i" ||
+      array[i] === "o" ||
+      array[i] === "u"
+    ) {
+      arrayVowel.push(array[i]);
+    }
+    i++;
+  }
+
+  return arrayVowel.length;
+}
+
+console.log(countVowels("Hello, my name is Julia"));
